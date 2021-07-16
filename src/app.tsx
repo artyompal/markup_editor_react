b73838ccee4a3f555222cb507cf76a89c33a1cc1
artyom palvelev <no@example.com>
2021-07-16 17:09:06 +0300

    renamed file

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import icon from '../assets/icon.svg';
-import './App.css';
+import './app.css';
 
 import { MainWindow } from 'components/MainWindow';
 
