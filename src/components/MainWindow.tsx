5adeb14a9feabd7ffd8ebfe96672f1ebe15a0dce
artyom palvelev <no@example.com>
2021-07-16 21:04:45 +0300

    generate spectrogram on demand

--- src/components/MainWindow.tsx
+++ src/components/MainWindow.tsx
@@ -1,7 +1,12 @@
-import React from 'react';
+import os from 'os';
+import path from 'path';
 
+import React from 'react';
 import AudioPlayer from 'react-h5-audio-player';
-import {ipcRenderer} from 'electron';
+
+import { ipcRenderer } from 'electron';
+import { spawnSync } from 'child_process';
+import { tmpNameSync } from 'tmp';
 
 import 'react-h5-audio-player/lib/styles.css'
 
@@ -9,24 +14,34 @@ import 'react-h5-audio-player/lib/styles.css'
 export class MainWindow extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {'file': undefined};
+    this.state = {mp3_file: undefined, spectrum_file: undefined};
     ipcRenderer.on('open_audio', (event, message) => this.open_audio(message));
   }
 
-  open_audio(filename: string) {
-      console.log('opening', filename);
-      this.setState({...this.state, 'file': 'file://' + filename});
+  open_audio(mp3_file: string) {
+      console.log('opening', mp3_file);
+
+      const spectrum_file = tmpNameSync({postfix: '.png'});
+      console.log('writing spectrogram to', spectrum_file);
+      spawnSync('python', ['gen_spectrogram.py', spectrum_file, '4_non_blondes.mp3'],
+        {cwd: '../ml_auto_scores/'});
+
+      this.setState({...this.state,
+        mp3_file: 'file://' + mp3_file, spectrum_file: 'file://' + spectrum_file});
   }
 
   render() {
-    if (!this.state.file) {
+    if (!this.state.mp3_file) {
       return null;
     }
 
     return (
       <div>
+        <svg>
+          <image href={this.state.spectrum_file} />
+        </svg>
         <div className="Application">
-          <AudioPlayer style={styles.player} src={this.state.file}/>
+          <AudioPlayer style={styles.player} src={this.state.mp3_file}/>
         </div>
       </div>
     );
