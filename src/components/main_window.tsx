ed9d4bde9179e946ed1ac505cf728f8f3b544656
artyom palvelev <no@example.com>
2021-07-20 13:41:08 +0300

    added playback cursor

--- src/components/main_window.tsx
+++ src/components/main_window.tsx
@@ -18,25 +18,31 @@ export default class MainWindow extends React.Component {
   constructor(props) {
     super(props);
     this.state = {mp3_file: undefined, spectrum_file: undefined,
+      duration: undefined, time: 0,
       spectrum_width: 0, spectrum_height: 0};
     this.player = React.createRef();
     ipcRenderer.on('open_audio', (event, message) => this.open_audio(message));
   }
 
   open_audio(mp3_file: string) {
-      console.log('opening', mp3_file);
+    console.log('opening', mp3_file);
 
-      const spectrum_file = tmpNameSync({postfix: '.png'});
-      console.log('writing spectrogram to', spectrum_file);
-      spawnSync('python', ['gen_spectrogram.py', spectrum_file, mp3_file],
-        {cwd: '../ml_auto_scores/'});
+    const spectrum_file = tmpNameSync({postfix: '.png'});
+    console.log('writing spectrogram to', spectrum_file);
+    spawnSync('python', ['gen_spectrogram.py', spectrum_file, mp3_file],
+      {cwd: '../ml_auto_scores/'});
 
-      const spectrum_sz = image_size(spectrum_file);
-      console.log('spectrum_sz', spectrum_sz);
+    const spectrum_sz = image_size(spectrum_file);
+    console.log('spectrum_sz', spectrum_sz);
 
-      this.setState({...this.state,
-        mp3_file: 'file://' + mp3_file, spectrum_file: 'file://' + spectrum_file,
-        spectrum_width: spectrum_sz.width, spectrum_height: spectrum_sz.height});
+    this.setState({...this.state,
+      mp3_file: 'file://' + mp3_file, spectrum_file: 'file://' + spectrum_file,
+      spectrum_width: spectrum_sz.width, spectrum_height: spectrum_sz.height});
+  }
+
+  on_playing() {
+    this.setState({...this.state, duration: this.player.current.audio.current.duration,
+                   time: this.player.current.audio.current.currentTime});
   }
 
   render() {
@@ -47,9 +53,13 @@ export default class MainWindow extends React.Component {
     return (
       <div>
         <Spectrogram spectrum_file={this.state.spectrum_file}
+          duration={this.state.duration} time={this.state.time}
           width={this.state.spectrum_width} height={this.state.spectrum_height} />
-        <AudioPlayer className="player" src={this.state.mp3_file} ref={this.player}
-          autoPlayAfterSrcChange={false} />
+        <AudioPlayer className="player" autoPlayAfterSrcChange={false}
+          src={this.state.mp3_file} ref={this.player}
+          onLoadedData={() => this.on_playing()} onListen={() => this.on_playing()}
+          listenInterval={16}
+          />
       </div>
     );
   }
--- src/components/spectrogram.tsx
+++ src/components/spectrogram.tsx
@@ -4,6 +4,7 @@ const SCALE_STEP = 0.5;
 const MIN_SCALE = 1.0;
 const MAX_SCALE = 50.0
 
+
 export default class Spectrogram extends React.Component {
   constructor(props) {
     super(props);
@@ -15,18 +16,18 @@ export default class Spectrogram extends React.Component {
     new_scale = Math.min(Math.max(new_scale, MIN_SCALE), MAX_SCALE);
 
     if (new_scale != this.state.scale) {
-      // console.log('scale', new_scale);
       this.setState({scale: new_scale});
     }
   }
 
   render() {
-    if (!this.props.spectrum_file) {
+    if (!this.props.spectrum_file || !this.props.duration) {
       return null;
     }
 
     const scale = this.state.scale;
     const w = this.props.width * scale, h = this.props.height;
+    const pos = w * this.props.time / this.props.duration;
 
     return (
       <div className="svg-outer-holder" onWheel={(e) => this.on_wheel(e)}>
@@ -34,6 +35,7 @@ export default class Spectrogram extends React.Component {
           <svg className="svg-block" width={w} >
             <image href={this.props.spectrum_file} preserveAspectRatio="none"
               x={0} y={0} width={w} height={h} />
+            <line className="cursor" y1={0} y2={10000} x1={pos} x2={pos} />
           </svg>
         </div>
       </div>
