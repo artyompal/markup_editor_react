2a26c8f1cc6711607edef816ca0462e61cac1925
artyom palvelev <no@example.com>
2021-08-12 12:05:14 +0300

    fixed tests

--- src/__tests__/undo_redo.test.ts
+++ src/__tests__/undo_redo.test.ts
@@ -8,26 +8,28 @@ describe('undo/redo', () => {
   });
 
   it('undo should work properly', () => {
-    let res = editor.create_file('package.json', [1, 2, 3, 4, 5], false);
-    expect(res).toStrictEqual({ bars: [1, 2, 3, 4, 5] });
+    editor.save_songs_table([{ mp3: 'some_song.mp3', tags: ['some', 'song'] }]);
+
+    let res = editor.create_file('some_song.mp3', [1, 2, 3, 4, 5], false);
+    expect(res).toStrictEqual({ bars: [1, 2, 3, 4, 5], final: false, start_offset: 0 });
 
     expect(editor.can_undo()).toBeFalsy();
     expect(editor.can_redo()).toBeFalsy();
 
     res = editor.filter_bars(1, 2);
-    expect(res).toStrictEqual({ bars: [2, 4] });
+    expect(res).toStrictEqual({ bars: [2, 4], final: false, start_offset: 0 });
 
     expect(editor.can_undo()).toBeTruthy();
     expect(editor.can_redo()).toBeFalsy();
 
     res = editor.undo();
-    expect(res).toStrictEqual({ bars: [1, 2, 3, 4, 5] });
+    expect(res).toStrictEqual({ bars: [1, 2, 3, 4, 5], final: false, start_offset: 0 });
 
     expect(editor.can_undo()).toBeFalsy();
     expect(editor.can_redo()).toBeTruthy();
 
     res = editor.redo();
-    expect(res).toStrictEqual({ bars: [2, 4] });
+    expect(res).toStrictEqual({ bars: [2, 4], final: false, start_offset: 0 });
 
     expect(editor.can_undo()).toBeTruthy();
     expect(editor.can_redo()).toBeFalsy();
